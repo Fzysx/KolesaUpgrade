97ed8c6f0d74edb82f5f6d2533ca2812d8f65e80
Fzysx <44568244+Fzysx@users.noreply.github.com>
2021-05-27 16:19:27 +0600

    lesson5_commit2

--- lesson_4/tests/acceptance/FindProductCest.php
+++ lesson_4/tests/acceptance/FindProductCest.php
@@ -9,14 +9,24 @@ class FindProductCest
 
     public function searchAndCheckProduct(AcceptanceTester $I)
     {    
+        $searchTextOfElementCSS= '#homefeatured > li:nth-child(2) .product-name';
+        $searchTextOfElementXPath = '//ul[@id="homefeatured"]/li[2]//a[@class="product-name"]'; 
+        $mouseOverElementCSS = '#homefeatured > li:nth-child(2) .product-name';
+        $mouseOverElementXPath = '//ul[@id="homefeatured"]/li[2]//a[@class="product-name"]';
+        $buttonClickCSS = '#homefeatured > li:nth-child(2) a.quick-view';
+        $buttonClickXPath = '//ul[@id="homefeatured"]/li[2]//a[@class="quick-view"]';
+        $iFrameCSS = '.fancybox-iframe';
+        $iFrameXPath = '//iframe[@class="fancybox-iframe"]';
+        $iFramesFindTextCSS = "#product h1";
+        $iFramesFindTextXPath = '//*[@id="product"]/div/div/div[2]/h1';
+
         $I->amOnPage('');
-        $I->waitForText('Blouse', 30, '#homefeatured > li:nth-child(2) > div > div.right-block > h5 > a');
-        $I->moveMouseOver('#homefeatured > li:nth-child(2) > div > div.right-block > h5 > a');
+        $I->waitForText('Blouse', 30, '#homefeatured > li:nth-child(2) .product-name');
+        $I->moveMouseOver('#homefeatured > li:nth-child(2) .product-name');
         $I->click('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view > span'); 
         $I->waitForElementVisible('.fancybox-iframe');
         $I->switchToIFrame('.fancybox-iframe');
-        $I->waitForText('Blouse', 30,'[itemprop="name"]');
-       
-
+        $I->waitForText('Blouse', 30,'#product h1');
+      
     }
 }
--- lesson_4/tests/functional/CountResultsCest.php
+++ lesson_4/tests/functional/CountResultsCest.php
@@ -8,15 +8,19 @@ class CountResultsCest
    
     public function checkSearchByText(FunctionalTester $I)
     {    
-        $searchInputFieldCSS = '';
-        $searchInputFieldXPath = ''; 
+        $searchInputFieldCSS = '#search_query_top';
+        $searchInputFieldXPath = '//input[@id="search_query_top"]'; 
+        $searchButtonCSS = '#searchbox > button';
+        $searchButtonXPath = '//button[@class="btn btn-default button-search"]';
+        $searchCountElementsCSS = '#center_column > ul > li';
+        $searchCountElementsXPath = '//div[@id="center_column"]/ul/li';
 
         $I->amOnPage('');
         $I->seeElement('#search_query_top');
         $I->click('#search_query_top');
         $I->fillField('#search_query_top', 'Printed dress');
         $I->click('#searchbox > button');
-        $I->seeNumberOfElements('div.left-block',5);
+        $I->seeNumberOfElements('#center_column > ul > li', 5);
 
         
        
